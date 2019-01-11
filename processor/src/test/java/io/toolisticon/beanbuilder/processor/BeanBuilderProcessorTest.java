81ddd8ae577a75e8b52cbaa84af6bfc637b371fc
Tobias Stamann <tobias.stamann@holisticon.de>
2019-01-11 16:03:28 +0100

    Moved to toolisticon maven parent pom. Switched form google to toolisticon compile-testing framework. Enhanced compatibility with java 9+ jdk

--- processor/src/test/java/io/toolisticon/beanbuilder/processor/BeanBuilderProcessorTest.java
+++ processor/src/test/java/io/toolisticon/beanbuilder/processor/BeanBuilderProcessorTest.java
@@ -1,136 +1,116 @@
 package io.toolisticon.beanbuilder.processor;
 
-import io.toolisticon.annotationprocessortoolkit.testhelper.AbstractAnnotationProcessorIntegrationTest;
-import io.toolisticon.annotationprocessortoolkit.testhelper.integrationtest.AnnotationProcessorIntegrationTestConfiguration;
-import io.toolisticon.annotationprocessortoolkit.testhelper.integrationtest.AnnotationProcessorIntegrationTestConfigurationBuilder;
 import io.toolisticon.annotationprocessortoolkit.tools.MessagerUtils;
 import io.toolisticon.annotationprocessortoolkit.tools.corematcher.CoreMatcherValidationMessages;
+import io.toolisticon.compiletesting.CompileTestBuilder;
+import io.toolisticon.compiletesting.JavaFileObjectUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.List;
 
 
 /**
  * Tests of {@link io.toolisticon.beanbuilder.api.BeanBuilder}.
  */
-@RunWith(Parameterized.class)
-public class BeanBuilderProcessorTest extends AbstractAnnotationProcessorIntegrationTest<BeanBuilderProcessor> {
+
+public class BeanBuilderProcessorTest {
 
 
-    public BeanBuilderProcessorTest(String description, AnnotationProcessorIntegrationTestConfiguration configuration) {
-        super(configuration);
-    }
+    CompileTestBuilder.CompilationTestBuilder compileTestBuilder;
 
     @Before
     public void init() {
         MessagerUtils.setPrintMessageCodes(true);
-    }
 
-    @Override
-    protected BeanBuilderProcessor getAnnotationProcessor() {
-        return new BeanBuilderProcessor();
-    }
-
-    @Parameterized.Parameters(name = "{index}: \"{0}\"")
-    public static List<Object[]> data() {
-
-        return Arrays.asList(new Object[][]{
-                {
-                        "Test valid usage",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseValidUsage.java")
-                                .compilationShouldSucceed()
-                                .build()
-                },
-                {
-                        "Test valid usage with custom classname",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseValidUsageWithCustomClassName.java")
-                                .compilationShouldSucceed()
-                                .build()
-                },
-                {
-                        "Test valid usage with custom packagename",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseValidUsageWithCustomPackage.java")
-                                .compilationShouldSucceed()
-                                .build()
-                },
-                {
-                        "Test valid usage with inheritance",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseValidUsageWithInheritance.java")
-                                .compilationShouldSucceed()
-                                .build()
-                },
-                {
-                        "Test valid usage without inheritance",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseValidUsageWithInheritance.java")
-                                .compilationShouldSucceed()
-                                .build()
-                },
-                {
-                        "Test invalid usage with abstract class",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseInvalidUsageAbstractClass.java")
-                                .compilationShouldFail()
-                                .addMessageValidator()
-                                .setErrorChecks(CoreMatcherValidationMessages.BY_MODIFIER.getCode())
-                                .finishMessageValidator()
-                                .build()
-                },
-                {
-                        "Test invalid usage without noarg constructor",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseInvalidUsageNoNoargConstructor.java")
-                                .compilationShouldFail()
-                                .addMessageValidator()
-                                .setErrorChecks(CoreMatcherValidationMessages.HAS_PUBLIC_NOARG_CONSTRUCTOR.getCode())
-                                .finishMessageValidator()
-                                .build()
-                },
-                {
-                        "Test invalid usage on enum",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseInvalidUsageOnEnum.java")
-                                .compilationShouldFail()
-                                .addMessageValidator()
-                                .setErrorChecks(CoreMatcherValidationMessages.IS_CLASS.getCode())
-                                .finishMessageValidator()
-                                .build()
-                },
-                {
-                        "Test invalid usage on interface",
-                        AnnotationProcessorIntegrationTestConfigurationBuilder
-                                .createTestConfig()
-                                .setSourceFileToCompile("testcases/TestcaseInvalidUsageOnInterface.java")
-                                .compilationShouldFail()
-                                .addMessageValidator()
-                                .setErrorChecks(CoreMatcherValidationMessages.IS_CLASS.getCode())
-                                .finishMessageValidator()
-                                .build()
-                },
-        });
-
+        compileTestBuilder = CompileTestBuilder
+                .compilationTest()
+                .addProcessors(BeanBuilderProcessor.class);
     }
 
 
     @Test
-    public void testCorrectnessOfAdviceArgumentAnnotation() {
-        super.test();
+    public void test_valid_usage() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseValidUsage.java"))
+                .compilationShouldSucceed()
+                .testCompilation();
+    }
+
+    @Test
+    public void test_valid_usage_with_custom_classname() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseValidUsageWithCustomClassName.java"))
+                .compilationShouldSucceed()
+                .testCompilation();
+    }
+
+
+    @Test
+    public void test_valid_usage_with_custom_packagename() {
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseValidUsageWithCustomPackage.java"))
+                .compilationShouldSucceed()
+                .testCompilation();
+    }
+
+
+    @Test
+    public void test_valid_usage_with_inheritance() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseValidUsageWithInheritance.java"))
+                .compilationShouldSucceed()
+                .testCompilation();
+    }
+
+    @Test
+    public void test_valid_usage_without_inheritance() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseValidUsageWithInheritance.java"))
+                .compilationShouldSucceed()
+                .testCompilation();
+    }
+
+    @Test
+    public void test_invalid_usage_with_abstract_class() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseInvalidUsageAbstractClass.java"))
+                .compilationShouldFail()
+                .expectedErrorMessages(CoreMatcherValidationMessages.BY_MODIFIER.getCode())
+                .testCompilation();
+    }
+
+    @Test
+    public void test_invalid_usage_without_noarg_constructor() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseInvalidUsageNoNoargConstructor.java"))
+                .compilationShouldFail()
+                .expectedErrorMessages(CoreMatcherValidationMessages.HAS_PUBLIC_NOARG_CONSTRUCTOR.getCode())
+                .testCompilation();
+    }
+
+    @Test
+    public void test_invalid_usage_on_enum() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseInvalidUsageOnEnum.java"))
+                .compilationShouldFail()
+                .expectedErrorMessages(CoreMatcherValidationMessages.IS_CLASS.getCode())
+                .testCompilation();
+    }
+
+    @Test
+    public void test_Test_invalid_usage_on_interface() {
+
+        compileTestBuilder
+                .addSources(JavaFileObjectUtils.readFromResource("testcases/TestcaseInvalidUsageOnInterface.java"))
+                .compilationShouldFail()
+                .expectedErrorMessages(CoreMatcherValidationMessages.IS_CLASS.getCode())
+                .testCompilation();
     }
 
 
