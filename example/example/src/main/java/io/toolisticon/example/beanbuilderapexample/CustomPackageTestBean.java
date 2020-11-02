bda9bed83069bf7492c0436b730106100a79ba43
Tobias Stamann <tobias.stamann@holisticon.de>
2020-11-02 12:53:36 +0100

    Try to fix release issues

--- example/example/src/main/java/io/toolisticon/example/beanbuilderapexample/CustomPackageTestBean.java
+++ example/example/src/main/java/io/toolisticon/example/beanbuilderapexample/CustomPackageTestBean.java
@@ -3,7 +3,7 @@ package io.toolisticon.example.beanbuilderapexample;
 import io.toolisticon.beanbuilder.api.BeanBuilder;
 import lombok.Data;
 
-@BeanBuilder(packageName = "io.toolisticon.murks")
+@BeanBuilder(packageName = "io.toolisticon.example.murks")
 @Data
 public class CustomPackageTestBean {
     private Long longField;
